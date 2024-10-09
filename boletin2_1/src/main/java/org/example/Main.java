7664998be8377223abe8337e15b17b76704c1462
CarlosCA1 <ccostasalonso@danielcastelao.org>
2024-10-09 13:48:49 +0200

    Resuelto

---
+++ boletin2_1/src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int base, altura, area;
+        base = 4;
+        altura = 3;
+        area = base * altura / 2;
+        System.out.println(area);
+    }
+}
