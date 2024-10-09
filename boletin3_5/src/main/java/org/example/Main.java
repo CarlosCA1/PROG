96618ffe22643c4adeb4e13de837d5c2baccfa70
CarlosCA1 <ccostasalonso@danielcastelao.org>
2024-10-09 14:25:35 +0200

    Resuelto

---
+++ boletin3_5/src/main/java/org/example/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Introduce un número: ");
+        double num1 = scanner.nextDouble();
+        System.out.print("Introduce outro número: ");
+        double num2 = scanner.nextDouble();
+        System.out.print("Introduce outro número: ");
+        double num3 = scanner.nextDouble();
+
+        if (num1>num2) {
+            if (num1>num3)
+                System.out.println("O maior é " + num1);
+        }
+
+        if (num2>num1) {
+            if (num2>num3)
+                System.out.println("O maior é " + num2);
+        }
+
+        if (num3>num2) {
+            if (num3>num1)
+                System.out.println("O maior é " + num3);
+        }
+        scanner.close();
+    }
+}
