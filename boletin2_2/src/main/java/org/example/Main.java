8a628de8e98fb2524fecf2d8b7a32df6602bb435
CarlosCA1 <ccostasalonso@danielcastelao.org>
2024-10-09 14:04:35 +0200

    Resuelto

---
+++ boletin2_2/src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int lado, area;
+        lado = 3;
+        area = (int) Math.pow (3, 2);
+        System.out.println(area);
+    }
+}
