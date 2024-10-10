d59e3092897cd438bc1585a03873d71c77c7d642
CarlosCA1 <ccostasalonso@danielcastelao.org>
2024-10-10 14:10:37 +0200

    Resuelto

---
+++ boletin2_extra_1/src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+            System.out.print("Prezo da tarifa: ");
+            double prezo_tarifa = scanner.nextDouble();
+            System.out.print("Prezo pagado: ");
+            double prezo_pagado = scanner.nextDouble();
+                double prezo_total = prezo_tarifa + prezo_pagado;
+                double porcentaxe = (prezo_tarifa * 100) / prezo_total;
+        System.out.println("Porcentaxe descontada: " + porcentaxe + "%");
+        scanner.close();
+
+    }
+}
