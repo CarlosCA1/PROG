fb6e06813316470650c1a3d57268be7d7743cda4
CarlosCA1 <ccostasalonso@danielcastelao.org>
2024-10-16 12:39:33 +0200

    Resuelto

---
+++ Notas/NOTAS2.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Introduzca un número entero entre 0 y 99: ");
+        int numero = scanner.nextInt();
+
+        int unidades;
+
+        switch (unidades) {
+            case 1:
+                System.out.println("uno");
+                break;
+            case 2:
+                System.out.println("dos");
+                break;
+            case 3:
+                System.out.println("tres");
+                break;
+            case 4:
+                System.out.println("cuatro");
+                break;
+            case 5:
+                System.out.println("cinco");
+                break;
+            case 6:
+                System.out.println("seis");
+                break;
+            case 7:
+                System.out.println("siete");
+                break;
+            case 8:
+                System.out.println("ocho");
+                break;
+            case 9:
+                System.out.println("nueve");
+                break;
+
+
+                if numero = 20 + switch (unidades) {
+                System.out.println("veinti" + switch (unidades));
+            }
+    }
+}
---
+++ boletin2/src/main/java/org/example/a.java
@@ -0,0 +1,9 @@
+package org.example;
+public class Main {
+    public static void main(String[] args) {
+        int lado, area;
+        lado = 3;
+        area = (int) Math.pow (3, 2);
+        System.out.println(area);
+    }
+}
