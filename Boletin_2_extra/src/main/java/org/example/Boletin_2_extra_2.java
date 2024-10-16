1d3e866bc22c23bb2871523a22a7404f80055fad
CarlosCA1 <ccostasalonso@danielcastelao.org>
2024-10-16 13:18:12 +0200

    Resuelto

---
+++ Boletin_2_extra/src/main/java/org/example/Boletin_2_extra_2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Boletin_2_extra_2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Temperatura (ºC): ");
+        double celsius = scanner.nextDouble();
+        double fahrenheit = (celsius * 9 / 5) + 32;
+        double kelvin = celsius + 273.15;
+        System.out.println("Temperatura en Fahrenheit: " + fahrenheit + "ºF");
+        System.out.println("Temperatura en Kelvin: " + kelvin + "K");
+        scanner.close();
+    }
+}
---
+++ Boletin_2_extra/src/main/java/org/example/Boletin_2_extra_5.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Boletin_2_extra_5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Sueldo fijo: ");
+        double sueldo_fijo = scanner.nextDouble();
+        System.out.print("Importe total de ventas: ");
+        double importe_ventas = scanner.nextDouble();
+        System.out.print("Kilómetros recorridos: ");
+        double km_recorridos= scanner.nextDouble();
+        System.out.print("Días de desplazamiento: ");
+        double dias_desplazamiento= scanner.nextDouble();
+        double comision = (5 / 100) * importe_ventas;
+        double kilometraje = 2 * km_recorridos;
+        double dietas = 30 * dias_desplazamiento;
+        double sueldo_bruto = sueldo_fijo - comision - kilometraje - dietas;
+        System.out.println("Sueldo bruto:" + sueldo_bruto + "€");
+        double descuento_irpf = 18 * sueldo_bruto / 100;
+        double irpf = sueldo_bruto - descuento_irpf;
+        double sueldo_liquido = irpf - 36;
+        System.out.println("Sueldo líquido: " + sueldo_liquido + "€");
+
+        scanner.close();
+    }
+}
