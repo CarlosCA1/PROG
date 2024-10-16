42214425216b615eef2869dc975556812a68120c
CarlosCA1 <ccostasalonso@danielcastelao.org>
2024-10-16 13:11:58 +0200

    Resuelto

---
+++ Boletin_3/src/main/java/org/example/Boletin_3_2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Boletin_3_2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Introduce un número: ");
+        short num1 = Short.reverseBytes(scanner.nextShort());
+        System.out.println("Introduce outro número: ");
+        short num2 = Short.reverseBytes(scanner.nextShort());
+
+        if (num1 >= num2) {
+            System.out.println(num1 - num2);
+        }
+        System.out.println(num1 + num2);
+        scanner.close();
+    }
+}
