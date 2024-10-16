77561da45210d0a3558376bb39aa7083e07c1178
CarlosCA1 <ccostasalonso@danielcastelao.org>
2024-10-16 12:41:00 +0200

    Resuelto

---
+++ Boletin3/src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Introduce un número: ");
+        double num1 = scanner.nextDouble();
+        if (num1 > 0) {
+            System.out.println(num1 + " é positivo");
+        }
+    }
+}
