da7785513f2453cbf0db8eee44d99365bd96bba9
CarlosCA1 <ccostasalonso@danielcastelao.org>
2024-12-05 12:25:54 +0100

    Resuelto

---
+++ horarioClase/src/Main.java
@@ -0,0 +1,37 @@
+public class Main {
+        public static String horarioCurso (String dia, int hora) {
+            System.out.println(horarioCurso("lunes", 5));
+            String[][] horario = {
+                    {"BD", "BD", "BD", "BD", "REC", "PROG", "PROG", "PROG"},
+                    {"BD", "BD", "BD", "SASP", "REC", "SI", "SI", "IPE"},
+                    {"PROG", "PROG", "PROG", "LM", "REC", "LM", "COD", "COD"},
+                    {"ING", "ING", "SI", "SI", "REC", "PROG", "PROG", "PROG"},
+                    {"COD", "LM", "LM", "SI", "REC", "SI", "IPE", "IPE"}
+            };
+            int numDia;
+            switch (dia) {
+                case "lunes" -> numDia = 0;
+                case "martes" -> numDia = 1;
+                case "miércoles" -> numDia = 2;
+                case "jueves" -> numDia = 3;
+                case "viernes" -> numDia = 4;
+                default -> numDia = -1;
+            }
+            if (numDia < 0 || numDia >= 5) {
+                return "Introduce un día entre lunes y viernes";
+            } else if (hora <= 0 || hora > 8) {
+                return "Introduce una hora entre 1 y 8";
+            } else
+                return horario[numDia][hora];
+        }
+        public static int horasModulo (String horario[][], String modulo) {
+            int cantHoras = 0;
+            for (int dia =0; dia<5; dia++) {
+                for (int hora=0; hora<=8; hora++) {
+                        if (modulo.equals(horario[dia][hora])){
+                            cantHoras++;
+                        }
+            }
+    }
+            return cantHoras;
+}}
