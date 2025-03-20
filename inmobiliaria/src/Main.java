6122b8708533627d32a73ddcc175e249e3ac7092
CarlosCA1 <ccostasalonso@danielcastelao.org>
2025-03-20 08:45:15 +0100

    Agregando Main.java

---
+++ inmobiliaria/src/Main.java
@@ -0,0 +1,8 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        PrazaGaraxe prazaGaraxe = new PrazaGaraxe("García Barbón", 12, "Vigo", 100.0, true, 4.2, 5.5, 15);
+        Vivenda pisoGBarbon = new  Vivenda("García Barbón", 12, "Vigo", 1200.0, true, "2", 3, "Non", "a", "3");
+    }
+}
