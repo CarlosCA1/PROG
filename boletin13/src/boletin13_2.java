337dc137f2762cbde9551d8859eba0473179d728
CarlosCA1 <ccostasalonso@danielcastelao.org>
2025-05-21 19:35:10 +0200

    Update boletin13_2.java

--- boletin13/src/boletin13_2.java
+++ boletin13/src/boletin13_2.java
@@ -1,25 +1,21 @@
 import java.util.*;
 
 public class boletin13_2 {
-
     public static void main(String[] args) {
-        List<Integer> numeros = new ArrayList<>();
-        Random random = new Random();
+        Collection<Integer> numeros = new ArrayList<>();
+        Random rand = new Random();
 
-        // Xeramos 100 números aleatorios entre 1 e 10
+        // Inserir 100 números aleatorios entre 1 e 10
         for (int i = 0; i < 100; i++) {
-            numeros.add(random.nextInt(10) + 1); // (0-9) + 1 => 1 a 10
+            numeros.add(rand.nextInt(10) + 1);
         }
 
-        // Mostramos a colección orixinal
-        System.out.println("Colección orixinal (100 números aleatorios):");
-        System.out.println(numeros);
+        System.out.println("Antes da eliminación: " + numeros);
 
-        // Eliminamos todos os 5 e 7
+        // Eliminar todos os 5 e 7
         numeros.removeIf(n -> n == 5 || n == 7);
 
-        // Mostramos a colección despois da eliminación
-        System.out.println("\nColección despois de eliminar 5 e 7:");
-        System.out.println(numeros);
+        System.out.println("Despois da eliminación: " + numeros);
     }
 }
+
